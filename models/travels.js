b63645916bc5fda319319375a8398051c4f7bef8
tbduong <tiffaniduong@gmail.com>
2016-07-15 15:35:31 -0700

    create model and schema, Travel db

---
+++ models/travels.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+var TravelSchema = new Schema({
+  description: String,
+  city: String,
+  country: String,
+  year_visited: String,
+  video: String, //attach link to vimeo.com
+  image: Image
+
+});
+
+var Travel = mongoose.model('Travel', TavelSchema); //set travel model to variable 'Travel'
+
+module.exports = Travel;  //allows other files to access 'Travel' database
