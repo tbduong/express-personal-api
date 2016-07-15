c67eaf08c176f84fe4b5f9a5092dd96add733428
tbduong <tiffaniduong@gmail.com>
2016-07-15 15:10:43 -0700

    modified documentation_url in server.js

--- server.js
+++ server.js
@@ -47,14 +47,15 @@ app.get('/api', function api_index(req, res) {
   res.json({
     woops_i_has_forgot_to_document_all_my_endpoints: true, // CHANGE ME ;)
     message: "Welcome to my personal api! Here's what you need to know!",
-    documentation_url: "https://github.com/example-username/express_self_api/README.md", // CHANGE ME
+    documentation_url: "https://github.com/tbduong/express_self_api/README.md", // CHANGE ME
     base_url: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
     endpoints: [
       {method: "GET", path: "/api", description: "Describes all available endpoints"},
-      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
+      {method: "GET", path: "/api/profile", description: "Data about me!"}, // CHANGE ME
+      {method: "GET", path: "/api/travels", description: "Shows all (most) of the places I have traveled to"},
       {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
     ]
-  })
+  });
 });
 
 /**********
