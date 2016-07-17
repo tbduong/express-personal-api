1fd3852d62a6b857c6da6300071b1a4e08eb48f6
tbduong <tiffaniduong@gmail.com>
2016-07-17 15:22:18 -0700

    begin setup for  handlebars

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,15 @@
 console.log("Sanity Check: JS is working!");
+var template;
+var $travelsList;
+var allTravels = [];
+
 $(document).ready(function(){
 
+$travelsList = $('#travelTarget');
 
-
-
-
-
+//comple handlebars template
+var source = $("#travels-template").html();
+template = Handlebars.compile(source);
 
 // your code
 
